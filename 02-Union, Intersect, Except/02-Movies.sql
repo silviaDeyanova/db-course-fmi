81a9b44bd6cca8dd8c1f840c30b79bcbb23389b1
silviaDeyanova <63786822+silviaDeyanova@users.noreply.github.com>
2021-04-22 17:51:15 +0300

    Update 02-Movies.sql

--- 02-Union, Intersect, Except/02-Movies.sql
+++ 02-Union, Intersect, Except/02-Movies.sql
@@ -1,5 +1,4 @@
---1 Напишете заявка, която извежда имената на актьорите мъже, участвали във 
-филма The Usual Suspects.
+--1. Напишете заявка, която извежда имената на актьорите мъже, участвали във филма The Usual Suspects.
 select name
 from moviestar
 where gender='m'
@@ -8,26 +7,26 @@ select starname
 from starsin
 where movietitle = 'The Usual Suspects';
 
---2
+--2. Напишете заявка, която извежда имената на актьорите, участвали във филми от 1995, продуцирани от студио MGM.
 select distinct starname
 from starsin, movie
 where movieyear=1995 and studioname='MGM' and movietitle=title;
 
---3
+--3. Напишете заявка, която извежда имената на продуцентите, които са продуцирали филми на студио MGM.
 select distinct name
 from movieexec join movie on studioname='MGM' and cert#=producerc#;
 
---4
+--4. Напишете заявка, която извежда имената на всички филми с дължина, поголяма от дължината на филма Star Wars.
 select m1.title
 from movie as m1, movie as m2
 where m1.length>m2.length and m2.title='Star Wars';
 
---5
+--5. Напишете заявка, която извежда имената на продуцентите с нетни активи поголеми от тези на Stephen Spielberg.
 SELECT EX1.NAME
 FROM MOVIEEXEC AS EX1 JOIN MOVIEEXEC AS EX2 ON (EX1.NETWORTH > EX2.NETWORTH)
 WHERE EX2.NAME = 'Stephen Spielberg';
 
---6
+--6. Напишете заявка, която извежда имената на всички филми, които са продуцирани от продуценти с нетни активи по-големи от тези на Stephen Spielberg.
 SELECT movie.title
 FROM MOVIEEXEC AS EX1 JOIN MOVIEEXEC AS EX2 ON (EX1.NETWORTH > EX2.NETWORTH) JOIN MOVIE ON (MOVIE.PRODUCERC# = EX1.CERT#)
 WHERE EX2.NAME = 'Stephen Spielberg';
